469f4e6d1767ab25c34799d148fa8ad31fd98757
Ariel Costa <acosta@adedesign.com>
2019-07-17 20:59:06 -0400

    Set up laravel auth

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('home')->with(['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Add some validation here.
+        $newPost = new Post();
+        $newPost->title = $request->title;
+        $newPost->body = $request->body;
+        $newPost->save();
+
+        return redirect("/home");
+    }
+
+    public function blog()
+    {
+        $posts = Post::all();
+        return view('posts')->with(['posts' => $posts]);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,66 +1,32 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-  <title>So fresh & so clean, clean</title>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <span>Dashboard</span>
+                    <a class="btn btn-primary" href="/create" style="float: right;">
+                        Add Post
+                    </a>
+                </div>
 
-{{--  <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto+Slab&display=swap" rel="stylesheet">--}}
-  <!-- Styles -->
-  <link href="{{ mix("css/app.css") }}"
-        rel="stylesheet" type="text/css"/>
-</head>
-<body>
-  <div class="content">
-    <nav class="navbar navbar-expand-md">
-      <a class="navbar-brand" href="#">AC</a>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-      <div class="navbar-collapse collapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/blog">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="centered-container">
-      <section class="title">
-        <span class="hello">Hello my friend,</span><br>
-        <span class="ariel">I'm Ariel.</span>
-      </section>
-      <section class="paragraph">
-        <p>
-          I'm a developer.  Here's where I'm supposed to tell you that my passion is writing concise code and saving
-          the world, one webpage at a time.  While I certainly love those things, really I'm typically just looking
-          forward to my next ice cream cone.
-        </p>
-      </section>
-      <section class="social">
-        <ul class="list-inline">
-          <li class="list-inline-item social-link">
-            <a href="https://twitter.com/arielbear99">
-              <img class="social-icon-img" src="{{ asset('storage/icon-twitter.png') }}" alt="twitter-icon">
-            </a>
-          </li>
-          <li class="list-inline-item social-link">
-            <a href="https://www.linkedin.com/in/arielmcosta/">
-              <img class="social-icon-img" src="{{ asset('storage/icon-linkedin.png') }}" alt="linkedin-icon">
-            </a>
-          </li>
-          <li class="list-inline-item social-link">
-            <a class="mail-link"
-               href="mailto:hello@arielcosta.com">
-              <img class="social-icon-img" src="{{ asset('storage/icon-mailbox.png') }}" alt="mail-icon">
-            </a>
-          </li>
-        </ul>
-      </section>
+                    <ul>
+                        @foreach($posts as $post)
+                            <li>{{ $post->title }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</body>
-</html>
+</div>
+@endsection
